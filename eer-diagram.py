b9995ece8a1473f403d48d0e910816092e72e1bd
Alec Krsek <aleckrsek@gmail.com>
2025-01-26 20:42:53 -0800

    Fix styling and add some entities/relations

--- eer-diagram.py
+++ eer-diagram.py
@@ -5,33 +5,49 @@ from graphviz import Digraph
 
 
 diagram = Digraph("Aiport EER Diagram", format="png")
-diagram.attr(rankdir="LR")
+diagram.attr(rankdir="TB")
 
 # Entities with their attributes
 entities = {
     "Airplane": ["Rnum"],
+    "PlaneType": ["Model", "Year", "Capacity", "Location"],
+    "Hangar": ["Hnum", "Capacity", "Location", "Hyear"],
+    "Maintenance": ["Date", "Workcode", "Hours", "Cost"],
+    "Owner": [],
+
 }
 
 # Loop to add the entities to the diagram
 for entity, attributes in entities.items():
-    diagram.node(entity, f"{entity}\n{attributes}")
+    label = f"<<TABLE BORDER='0' CELLBORDER='1' CELLSPACING='0'>"
+    label += f"<TR><TD COLSPAN='1'><B>{entity}</B></TD></TR>"
+    for attr in attributes:
+        label += f"<TR><TD>{attr}</TD></TR>"
+    label += "</TABLE>>"
+    diagram.node(entity, label=label, shape="plaintext")
 
 
 # Relationships
 relationships = {
     "OF-TYPE": ("Airplane", "PlaneType"),
     "STORED-IN": ("Airplane", "Hangar"),
+    "OWNS": ("Airplane", "Owner"),
+    "MAINTAIN":("Employee", "Maintenance"),
+    "PLANE-MAINTAINCE": ("Airplane", "Maintenance"),
+
 }
 
 # Add relationships to the diagram
 for rel, (from_ent, to_ent), in relationships.items():
-    diagram.edge(from_ent, to_ent, label=rel)
+    diagram.node(rel, rel, shape="diamond")
+    diagram.edge(from_ent, rel)
+    diagram.edge(rel, to_ent)
 
 # Specializations and Generalizations
-diagram.node("Person", "Peson\nPid, name, Address, Phone, Dob", shape="ellipse")
+diagram.node("Person", "Person", shape="ellipse")
 
-# Union for Owner
-diagram.node("Owner", "Owner", shape="ellipse")
+# Unions
+diagram.node("OwnerUnion", "U", shape="circle")
 
 # Add the diagram to the file
 file_path = "eer-diagram"
