268d6c7c984ea9822c416cf851db265460449dd3
Alec Krsek <aleckrsek@gmail.com>
2025-01-26 21:50:44 -0800

    Complete diagram

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ER diagram for an aiport.
 Graphviz not available for Pthon 3.13+
+
+Sadly the Union symbols were added with a photo editing software.
+Thick lines are mean to be double lines
+
+## Submission
+Submission is located in Extended-ER-Diagram.png
--- eer-diagram.py
+++ eer-diagram.py
@@ -9,11 +9,15 @@ diagram.attr(rankdir="TB")
 
 # Entities with their attributes
 entities = {
-    "Airplane": ["Rnum"],
+    "Airplane": ["<U>Rnum</U>"],
     "PlaneType": ["Model", "Year", "Capacity", "Location"],
-    "Hangar": ["Hnum", "Capacity", "Location", "Hyear"],
+    "Hangar": ["<U>Hnum</U>", "Capacity", "Location", "Hyear"],
     "Maintenance": ["Date", "Workcode", "Hours", "Cost"],
     "Owner": [],
+    "Company": ["<U>Ein</U>", "Name", "Address", "Phone"],
+    "Person": ["<U>Pid</U>", "Name", "Address", "Phone", "Dob"],
+    "Pilot": ["<U>Lnum</U>", "Plevel", "Restrict"],
+    "Employee": ["Salary", "Shift", "Hdate"],
 
 }
 
@@ -24,32 +28,47 @@ for entity, attributes in entities.items():
     for attr in attributes:
         label += f"<TR><TD>{attr}</TD></TR>"
     label += "</TABLE>>"
-    diagram.node(entity, label=label, shape="plaintext")
+    diagram.node(entity, label=label, shape="none")
 
 
 # Relationships
 relationships = {
-    "OF-TYPE": ("Airplane", "PlaneType"),
-    "STORED-IN": ("Airplane", "Hangar"),
-    "OWNS": ("Airplane", "Owner"),
-    "MAINTAIN":("Employee", "Maintenance"),
-    "PLANE-MAINTAINCE": ("Airplane", "Maintenance"),
-
+    "OF-TYPE": ("Airplane", "PlaneType", "1", "N"),
+    "STORED-IN": ("Airplane", "Hangar", "1", "N"),
+    "OWNS": ("Airplane", "Owner", "M", "N"),
+    "MAINTAIN":("Employee", "Maintenance", " 1", "N"),
+    "PLANE-MAINTAINCE": ("Airplane", "Maintenance", "1", "N"),
+    "FLIES": ("Pilot", "PlaneType", "M", "N"),
+    "WORKS-ON": ("Employee", "PlaneType", "M", "N"),
 }
 
+totalRelations= {"OWNS", "STORED-IN", "OF-TYPE", "FLIES", "WORKS-ON"}
+
 # Add relationships to the diagram
-for rel, (from_ent, to_ent), in relationships.items():
-    diagram.node(rel, rel, shape="diamond")
-    diagram.edge(from_ent, rel)
-    diagram.edge(rel, to_ent)
+for rel, (from_ent, to_ent, from_card, to_card), in relationships.items():
+    def double_line():
+        diagram.node(rel, rel, shape="diamond")
+        diagram.edge(from_ent, rel, dir="none", label=f"{from_card}", penwidth="2")
+        diagram.edge(rel, to_ent, dir="none", label=f"{to_card}")     
+    if rel in totalRelations:
+        double_line()
+    else:
+        diagram.node(rel, rel, shape="diamond")
+        diagram.edge(from_ent, rel, dir="none", label=f"{from_card}")
+        diagram.edge(rel, to_ent, dir="none", label=f"{to_card}")
 
 # Specializations and Generalizations
-diagram.node("Person", "Person", shape="ellipse")
+diagram.node("PersonSubclass", "d", shape="circle") # The union circle with the label d
+diagram.edge("Pilot", "PersonSubclass", dir="none")
+diagram.edge("Employee", "PersonSubclass", dir="none")
+diagram.edge("PersonSubclass", "Person", dir="none", penwidth="2")
 
 # Unions
-diagram.node("OwnerUnion", "U", shape="circle")
+diagram.node("OwnerUnion", "U", shape="circle") # The union circle with the label U
+diagram.edge("OwnerUnion", "Person", dir="none")
+diagram.edge("OwnerUnion", "Company", dir="none")
+diagram.edge("OwnerUnion", "Owner", dir="none", penwidth="2")
 
 # Add the diagram to the file
-file_path = "eer-diagram"
+file_path = "pyExport"
 diagram.render(file_path, cleanup=True)
-
